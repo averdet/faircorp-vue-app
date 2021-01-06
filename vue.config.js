33f91a95135435d3874413436050e55166a0e638
Alexandre <alexandre.verdet@etu.emse.fr>
2021-01-06 17:59:25 +0100

    generalize ci

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     lintOnSave: false,
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/faircorp-vue-app/'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/'
 }
